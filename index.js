d38900e8d8e053f43bcea15a800e0e665f40a7d3
OOP <opkampitak@hotmail.com>
2016-10-14 14:16:43 +0700

    Auth using SMTP if BasicAuth doesn't work

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+'use strict';
 const axios=require('axios');
 const https=require('https');
+const SMTPConnection = require('smtp-connection');
 module.exports.auth = function(user,pass){
     return axios.get('https://www.it.chula.ac.th/downloads',{
         auth:{
@@ -20,5 +22,27 @@ module.exports.auth = function(user,pass){
             default:
                 return Promise.reject(error);
         }
+    })
+    .catch(error=>{
+        let connection = new SMTPConnection({
+            host:'webmail.student.chula.ac.th'
+        });
+        return new Promise(function(resolve, reject) {
+            connection.on('error',e=>reject(e));
+            connection.connect( ()=>{
+                connection.login({
+                    user:user,
+                    pass:pass
+                }, e=>{
+                    connection.close();
+                    if(e){
+                        resolve(false);
+                    }
+                    else{
+                        resolve(true);
+                    }
+                });
+            });
+        });
     });
 }
